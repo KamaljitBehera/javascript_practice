b5b5019429ee1909c0f4fc945a9d5fe8201838ea
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-12 15:16:22 +0530

    Basic of javascript is added successfully

---
+++ Basic.js
@@ -0,0 +1,63 @@
+// thsi statement below is used to demonstrate the external way to wite a javascript language
+console.log("This message is print in extenal way");
+
+
+// Below this showing how to use semicolon in javascript and is it mandatory to use semiclone or not
+
+// let add two number
+var a=10//semicolon use is not mandatory but if we use strict mode then we have to use semicolon
+var b=20;
+console.log("The addition of a and b is "+(a+b));
+
+
+// Below code showing how to write a function or block of code
+function myFuction(){
+    console.log("Hello There");
+    console.log("This is my first function");
+}
+// calling the fuction is mandatory
+myFuction();
+
+// below code show that is javascript ignore the white space
+console.log(a       *        b);//yes javascript ignore the white space
+
+// The variable whose value is fixed is called literals
+
+const num=10;
+const name="Kamal";
+console.log(num);
+console.log(name);
+// try to change the value 
+/*num=20;
+name="jaga";
+*///here we get error
+console.log(num);
+console.log(name);
+
+
+//variable is refers to something whose value can be change
+
+var language="english";
+console.log(language);
+//change the value of language
+language="javascript";
+console.log(language);
+
+// the below code showing the syntax or arrangement of code 
+// Variable Declarations
+let x, num1, sum;
+
+// Assign value to the variables
+x = 20;
+y = 30
+
+// Expression to divide a number
+num1 = x / 2;
+
+// Expression to add two numbers
+sum = x + y;
+
+console.log(num1 + "\n" + sum);
+
+
+
